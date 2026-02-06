81734670aaf83590ab462cbf35278190b9d66b50
pcback <pcbackrmartinez@gmail.com>
2026-02-06 06:34:36 -0300

    feat: conexionDB.js      v.2 backend

---
+++ backend/db/conexionDB.js
@@ -0,0 +1,17 @@
+// backend/db/conexionDB.js
+import dotenv from "dotenv";
+dotenv.config();
+
+import pkg from "pg";
+const { Pool } = pkg;
+
+export const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: String(process.env.DB_PASSWORD), // 🔥 forzar string
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  allowExitOnIdle: true,
+});
+
+
