70343c52a77573beac53400a7ce524b692249f2e
pcback <pcbackrmartinez@gmail.com>
2026-02-06 06:30:46 -0300

    feat: usuarios  v.2 Frontend

---
+++ backend/consultas/usuarios.js
@@ -0,0 +1,55 @@
+//backend/consultas/usuarios.js
+
+import {pool } from "../db/conexionDB.js" 
+import bcrypt from "bcryptjs";
+
+//obtener usuario
+export const obtenerUsuarios = async () => {
+  const { rows } = await pool.query("SELECT * FROM usuarios");
+  return rows;
+};
+
+//ver usuario
+export const verUsuarios = async (id) => {
+  const consulta = "SELECT * FROM usuarios WHERE id = $1";
+  const { rows } = await pool.query(consulta, [id]);
+  return rows;
+};
+
+//eliminar usuario
+export const eliminarUsuario = async (id) => {
+  const consulta = `
+    DELETE FROM usuarios WHERE id = $1
+    RETURNING *`;
+  const values = [id];
+  const { rows } = await pool.query(consulta, values);
+  return rows[0];
+};
+
+//agregar usuario
+
+export const agregarUsuario = async (nombre, email, password, role) => {
+  const hash = await bcrypt.hash(password, 10);
+
+  const consulta = `
+    INSERT INTO usuarios (nombre, email, password, role)
+    VALUES ($1,$2,$3,$4)
+    RETURNING *`;
+
+  const values = [nombre, email, hash, role];
+
+  const { rows } = await pool.query(consulta, values);
+  return rows[0];
+};
+
+//modificar usuario
+export const modificarUsuarios = async (id, nombre, email, password, role) => {
+  const consulta = `
+    UPDATE usuarios SET nombre = $2, email = $3, password = $4, role = $5 WHERE id = $1
+    RETURNING *`;
+
+  const values = [id, nombre, email, password, role];
+  const { rows } = await pool.query(consulta, values);
+  return rows[0];
+};
+
